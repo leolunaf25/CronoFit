bc57fa661b62a7cd992baf1b70d7d53323654f04
leolunaf25 <lunalunaf25@gmail.com>
2024-10-27 23:56:55 -0600

    add updates resources

--- app/src/main/java/com/lunatcoms/cronofit/MainActivity.kt
+++ app/src/main/java/com/lunatcoms/cronofit/MainActivity.kt
@@ -1,43 +1,76 @@
 package com.lunatcoms.cronofit
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.lunatcoms.cronofit.databinding.ActivityMainBinding
+import android.os.Handler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import android.animation.ObjectAnimator
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var contador1:Int=10
-    private var contador2:Int=1
+    private var totalTime:Long = 30000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        startChronometer()
+        initUI()
 
-        binding.pbBarra.progress = 0
+        binding.pbBarra3.max = 100
+
+        val animator = ObjectAnimator.ofInt(binding.pbBarra3,"progress",50)
+        animator.duration = 2000
+        animator.start()
+
+
     }
 
-    private fun startChronometer(){
-        object : CountDownTimer(10000, 1000) {
+    private fun initUI() {
+        starChronometer()
+        startChronometerAndCounter()
+    }
 
-            override fun onTick(millisUntilFinished: Long) {
+    private fun starChronometer(){
+        CoroutineScope(Dispatchers.Main).launch {
+            var progress = 0
+            while (true) {
+                delay(totalTime / 100L)
+                progress += 1
+                binding.pbBarra.progress = progress
+                binding.pbBarraHorizontal.progress = progress
 
-                binding.tvChronometer.text = contador1.toString()
-                binding.pbBarra.progress = contador2*10
-                contador1--
-                contador2++
+                if (progress >= 100) {
+                    progress = 0
+                }
             }
+        }
+    }
 
-            override fun onFinish() {
-                contador1 = 10
-                contador2 = 1
-                startChronometer()
+    private fun startChronometerAndCounter() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val startTime = System.currentTimeMillis()
+
+            while (true) {
+                val elapsedTime = System.currentTimeMillis() - startTime
+
+                // Calcular el progreso en función del tiempo transcurrido
+                val progress = ((elapsedTime % totalTime) / totalTime.toDouble()) * 100
+                //binding.pbBarra2.progress = progress.toInt()
+                binding.pbBarraHorizontal2.progress = progress.toInt()
+
+                // Calcular el tiempo restante en segundos para el cronómetro
+                val timeCurrent = (totalTime - (elapsedTime % totalTime)) / 1000
+                binding.tvChronometer2.text = (timeCurrent+1).toString()
+
+                delay(17L)
             }
-        }.start()
-
+        }
     }
 }
